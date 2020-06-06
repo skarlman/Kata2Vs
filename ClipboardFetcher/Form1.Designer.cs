a0a4ab16cdc9668dbee52f3568e4bfb86414a81e
KarlT <karl.tillstrom@gmail.com>
2020-06-06 16:21:20 +0200

    missing files

---
+++ ClipboardFetcher/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ClipboardFetcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtCodePath = new System.Windows.Forms.TextBox();
+            this.btnCreateFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 97);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(341, 216);
+            this.textBox1.TabIndex = 0;
+            // 
+            // txtCodePath
+            // 
+            this.txtCodePath.Location = new System.Drawing.Point(12, 23);
+            this.txtCodePath.Name = "txtCodePath";
+            this.txtCodePath.Size = new System.Drawing.Size(361, 20);
+            this.txtCodePath.TabIndex = 1;
+            this.txtCodePath.Text = "C:\\code\\temp\\KataSolver\\NUnitTestProject1";
+            // 
+            // btnCreateFile
+            // 
+            this.btnCreateFile.Location = new System.Drawing.Point(174, 319);
+            this.btnCreateFile.Name = "btnCreateFile";
+            this.btnCreateFile.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateFile.TabIndex = 2;
+            this.btnCreateFile.Text = "button1";
+            this.btnCreateFile.UseVisualStyleBackColor = true;
+            this.btnCreateFile.Click += new System.EventHandler(this.btnCreateFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCreateFile);
+            this.Controls.Add(this.txtCodePath);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCodePath;
+        private System.Windows.Forms.Button btnCreateFile;
+    }
+}
+
---
+++ ClipboardFetcher/Form1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WK.Libraries.SharpClipboardNS;
+using static WK.Libraries.SharpClipboardNS.SharpClipboard;
+
+namespace ClipboardFetcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            clipboard.ClipboardChanged += ClipboardChanged;
+        }
+        SharpClipboard clipboard = new SharpClipboard();
+        private void ClipboardChanged(Object sender, ClipboardChangedEventArgs e)
+        {
+            // Is the content copied of text type?
+            if (e.ContentType == SharpClipboard.ContentTypes.Text)
+            {
+                var text = clipboard.ClipboardText;
+                if (!text.StartsWith("KATACODE:"))
+                    return;
+
+                text = text.Replace("\r", "");
+                text = text.Replace(((char)8203).ToString(), "");
+
+                var lines = text.Split('\n')
+                                .Skip(1)
+                                .Select((l, i) => i % 2 == 1 ? l : (string)null)
+                                .Where(l => l != null);
+
+
+
+
+                // Get the cut/copied text.
+                textBox1.Text = string.Join(Environment.NewLine, lines);
+            }
+
+            // Is the content copied of image type?
+            else if (e.ContentType == SharpClipboard.ContentTypes.Image)
+            {
+                // Get the cut/copied image.
+                //Image img = clipboard.ClipboardImage;
+            }
+
+            // Is the content copied of file type?
+            else if (e.ContentType == SharpClipboard.ContentTypes.Files)
+            {
+                // Get the cut/copied file/files.
+                //Debug.WriteLine(clipboard.ClipboardFiles.ToArray());
+
+                // ...or use 'ClipboardFile' to get a single copied file.
+                //Debug.WriteLine(clipboard.ClipboardFile);
+            }
+
+            // If the cut/copied content is complex, use 'Other'.
+            else if (e.ContentType == SharpClipboard.ContentTypes.Other)
+            {
+                // Do something with 'clipboard.ClipboardObject' or 'e.Content' here...
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreateFile_Click(object sender, EventArgs e)
+        {
+            var path = txtCodePath.Text;
+
+            var files = Directory.GetFiles(path, "Kata*.cs");
+            var nextFile = $"Kata{files.Count() + 1}.cs";
+
+            File.WriteAllText(Path.Combine(path, nextFile), textBox1.Text);
+        }
+    }
+}
